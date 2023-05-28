5b6fca4df273a91317600e5ec059a49e1e728600
Donia Ahmed <pppopppo33@gmail.com>
2023-05-28 02:02:44 +0200

    Lab 1

--- App.js
+++ App.js
@@ -1,12 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, SafeAreaView, ScrollView, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+      <View style={{width:393}}>
+        <Image source={require('./assets/4.jpg')}></Image>
+      </View>
+      <View style={{width:393}}>
+        <Image source={require('./assets/1.jpg')}></Image>
+      </View>
+      <View style={{width:393}}>
+        <Image source={require('./assets/2.jpg')}></Image>
+      </View>
+      <View style={{width:393}}>
+        <Image source={require('./assets/3.jpg')}></Image>
+      </View>
+    </ScrollView >
   );
 }
 
